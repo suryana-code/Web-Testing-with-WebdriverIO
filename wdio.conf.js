67870de79c264b74ca6b02d2b34512fea9e59c3c
suryana-code <msuryana45@gmail.com>
2025-05-23 13:54:33 +0700

    add github action for webdriverIO

--- wdio.conf.js
+++ wdio.conf.js
@@ -49,7 +49,14 @@ export const config = {
   //
   capabilities: [
     {
+      maxInstances: 1,
       browserName: "chrome",
+      "goog:chromeOptions": {
+        args:
+          process.env.HEADLESS === "true"
+            ? ["--headless", "--disable-gpu", "--window-size=1920,1080"]
+            : ["--disable-gpu", "--window-size=1920,1080"],
+      },
     },
   ],
 
@@ -100,7 +107,7 @@ export const config = {
   // Services take over a specific job you don't want to take care of. They enhance
   // your test setup with almost no effort. Unlike plugins, they don't add new
   // commands. Instead, they hook themselves up into the test process.
-  // services: [],
+  services: ["chromedriver"],
   //
   // Framework you want to run your specs with.
   // The following are supported: Mocha, Jasmine, and Cucumber
